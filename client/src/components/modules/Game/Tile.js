db5bccdab8629eccaa2f49ad877d8321866bd215
Eren Shin <eshin0525@gmail.com>
2021-01-16 13:33:15 -0800

    added tiles and mirrors

--- client/src/components/modules/Game/Tile.js
+++ client/src/components/modules/Game/Tile.js
@@ -10,7 +10,9 @@ class Tile extends Component {
   render() {
     return (
       <div className="Tile-container u-flex u-flex-justifyCenter u-flex-alignCenter">
-        <div className="Tile-body"></div>
+        <div className="Tile-body">
+          <div className=".empty"></div>
+        </div>
       </div>
     );
   }
