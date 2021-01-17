b909dbb37a95fb43fc90947d7acea01aac12edb3
Eren Shin <eshin0525@gmail.com>
2021-01-16 21:23:13 -0800

    mirrors

--- client/src/components/modules/Game/GameBoard.js
+++ client/src/components/modules/Game/GameBoard.js
@@ -8,68 +8,9 @@ class GameBoard extends Component {
     super(props);
   }
 
-  // character starting location, wall location, or goal location
-  invalidLoc = (x, y) => {
-    return (
-      (x === 0 && y === 4) ||
-      (x === 4 && y === 0) ||
-      (x === 4 && y === 8) ||
-      (x === 8 && y === 4) ||
-      (x === 4 && y === 4) ||
-      (x === 0 && y === 0) ||
-      (x === 0 && y === 8) ||
-      (x === 8 && y === 0) ||
-      (x === 8 && y === 8)
-    );
-  };
-  containsObj = (mirrors, { x, y }) => {
-    for (let i = 0; i < mirrors.length; i++) {
-      if (mirrors[i].x === x && mirrors[i].y === y) {
-        return true;
-      }
-    }
-    return false;
-  };
-
-  createMirrors = () => {
-    let possibleMirrors = [];
-    for (let i = 0; i < this.props.board.length; i++) {
-      for (let j = 0; j < this.props.board[i].length; j++) {
-        if (!this.invalidLoc(i, j)) {
-          possibleMirrors.push({ x: i + 1, y: j + 1 });
-        }
-      }
-    }
-    let mirrorsArr = [];
-    while (true) {
-      let mirrorNearby = false;
-      if (mirrorsArr.length === this.props.mirrors) {
-        break;
-      }
-      let randomLoc = possibleMirrors[Math.floor(Math.random() * possibleMirrors.length)];
-      for (let i of [-1, 1, 0]) {
-        for (let j of [-1, 1, 0]) {
-          if (this.containsObj(mirrorsArr, { x: randomLoc.x + i, y: randomLoc.y + j })) {
-            mirrorNearby = true;
-            break;
-          }
-        }
-        if (mirrorNearby) {
-          break;
-        }
-      }
-      if (mirrorNearby) {
-        continue;
-      }
-      mirrorsArr.push(randomLoc);
-    }
-    console.log(mirrorsArr);
-    return mirrorsArr;
-  };
-
   createTiles = () => {
     let tiles = [];
-    let mirrors = this.createMirrors();
+    let mirrors = this.props.mirrors;
     for (let i = 0; i < this.props.board.length; i++) {
       for (let j = 0; j < this.props.board[i].length; j++) {
         tiles.push(
--- client/src/components/pages/Game.js
+++ client/src/components/pages/Game.js
@@ -19,6 +19,7 @@ class Game extends Component {
       board: [],
       players: [],
       mirrors: 0,
+      mirrorsArr: [],
     };
   }
 
@@ -29,7 +30,66 @@ class Game extends Component {
       board: data.board,
       players: data.players,
       mirrors: data.mirrors,
+      mirrorsArr: [],
     });
+  };
+
+  invalidLoc = (x, y) => {
+    return (
+      (x === 0 && y === 4) ||
+      (x === 4 && y === 0) ||
+      (x === 4 && y === 8) ||
+      (x === 8 && y === 4) ||
+      (x === 4 && y === 4) ||
+      (x === 0 && y === 0) ||
+      (x === 0 && y === 8) ||
+      (x === 8 && y === 0) ||
+      (x === 8 && y === 8)
+    );
+  };
+
+  containsObj = (mirrors, { x, y }) => {
+    for (let i = 0; i < mirrors.length; i++) {
+      if (mirrors[i].x === x && mirrors[i].y === y) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  createMirrors = () => {
+    let possibleMirrors = [];
+    for (let i = 0; i < this.state.board.length; i++) {
+      for (let j = 0; j < this.state.board[i].length; j++) {
+        if (!this.invalidLoc(i, j)) {
+          possibleMirrors.push({ x: i + 1, y: j + 1 });
+        }
+      }
+    }
+    let mirrorsArray = [];
+    while (true) {
+      let mirrorNearby = false;
+      if (mirrorsArray.length === this.state.mirrors) {
+        break;
+      }
+      let randomLoc = possibleMirrors[Math.floor(Math.random() * possibleMirrors.length)];
+      for (let i of [-1, 1, 0]) {
+        for (let j of [-1, 1, 0]) {
+          if (this.containsObj(mirrorsArray, { x: randomLoc.x + i, y: randomLoc.y + j })) {
+            mirrorNearby = true;
+            break;
+          }
+        }
+        if (mirrorNearby) {
+          break;
+        }
+      }
+      if (mirrorNearby) {
+        continue;
+      }
+      mirrorsArray.push(randomLoc);
+    }
+    return mirrorsArray;
   };
 
   componentDidMount() {
@@ -41,6 +101,7 @@ class Game extends Component {
             this.update(game);
           });
         }
+        this.createMirrors();
       })
       .catch((err) => console.log(err));
   }
@@ -52,7 +113,7 @@ class Game extends Component {
           <Info roomName={this.state.roomName} roomCode={this.state.roomCode} />
         </div>
         <div className="board">
-          <GameBoard board={this.state.board} mirrors={this.state.mirrors} />
+          <GameBoard board={this.state.board} mirrors={this.createMirrors()} />
         </div>
         <div className="scores">
           <ScoreBoard players={this.state.players} />
