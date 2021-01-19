c8de50a6be3a8792e7e75f2d594250cae54bdf67
Eren Shin <eshin0525@gmail.com>
2021-01-19 15:12:20 -0800

    laser bug

--- server/api.js
+++ server/api.js
@@ -189,11 +189,6 @@ router.post("/laser", auth.ensureLoggedIn, (req, res) => {
           game.board = makeBoard.updateBoard(game.board, "", beam[1], att);
           game.players[game.players.indexOf(playersHit[0])].location = loc;
         }
-        game.save().then((data) => {
-          data.players.map((player) => {
-            socketManager.getSocketFromUserID(player.id).emit("updateBoard", data);
-          });
-        });
 
         game.board = makeBoard.resetLight(game.board);
         setTimeout(emit, 500);
